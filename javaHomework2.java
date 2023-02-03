1140c992b815cc171599e1c89a41937e8479d836
maxim_stoma <maksim.stoma@mail.ru>
2023-02-04 02:39:11 +0300

    implemented 3 methods: removeEvenValues, getMin, getMax

--- javaHomework2.java
+++ javaHomework2.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class javaHomework2 {
     public static void main(String[] args) {
@@ -8,5 +9,27 @@ public class javaHomework2 {
             list.add((int) (Math.random() * 100));
         }
         System.out.println("Список случайных чисел: " + list);
+
+        List<Integer> filteredList = removeEvenValues(list);
+        System.out.println("Список с удалёнными чётными числами: " + filteredList);
+
+        int minElement = getMin(filteredList);
+        System.out.println("Минимальный элемент массива: " + minElement);
+
+        int maxElement = getMax(filteredList);
+        System.out.println("Максимальный элемент массива: " + maxElement);
+
+    }
+
+    public static List<Integer> removeEvenValues(List<Integer> list) {
+        return list.stream().filter(i -> i % 2 != 0).collect(Collectors.toList());
+    }
+
+    public static Integer getMin(List<Integer> list) {
+        return list.stream().min(Integer::compareTo).orElse(0);
+    }
+
+    public static Integer getMax(List<Integer> list) {
+        return list.stream().max(Integer::compareTo).orElse(0);
     }
 }
